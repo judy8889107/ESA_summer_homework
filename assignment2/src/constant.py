26efb39d44e0a0ad6c6e16f0ca5c8734caf3b2d5
judy <judy8889107@gmail.com>
2023-03-11 09:21:37 +0800

    增加constant常量:放入folder的絕對路徑，方便切換 繪製圖表:讀取pkl檔案並繪製出

---
+++ assignment2/src/constant.py
@@ -0,0 +1,3 @@
+OUTPUT_DIR = "C:/Users/USER/Desktop/Master_0/ESC_LAB/assignment2/output"
+DATASET_DIR = "C:/Users/USER/Desktop/Master_0/ESC_LAB/assignment2/dataset"
+DATA_DIR = "C:/Users/USER/Desktop/Master_0/ESC_LAB/assignment2/data"
